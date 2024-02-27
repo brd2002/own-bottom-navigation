1a079ee2c2260718f4b84d6ac70dd5729f432efb
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-27 15:22:15 +0530

    compeleted navbar

--- app/src/main/java/com/example/bottomnavigationlearning/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationlearning/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.bottomnavigationlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.bottomnavigationlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottombar = findViewById<View>(R.id.fragmentContainerView)
+        val bottomNavigation = binding.bottomNavigationView
+        bottomNavigation.setupWithNavController(bottombar.findNavController())
+
     }
 }
